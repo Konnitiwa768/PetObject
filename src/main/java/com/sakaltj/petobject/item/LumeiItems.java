eb5f925c1fb588d7d67cbe7976ef536aceadf1a2
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-02 16:29:39 +0900

    Create LumeiItems.java

---
+++ src/main/java/com/sakaltj/petobject/item/LumeiItems.java
@@ -0,0 +1,21 @@
+package com.sakalti.petobject.item;
+
+import com.sakalti.petobject.entity.EntityRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class LumeiItems {
+    public static final Item LUMEI_SPAWN_EGG = new SpawnEggItem(
+        EntityRegistry.LUMEI,
+        0x222222,  // 黒に近い本体色
+        0xffcc00,  // 明るい黄ランタンの色
+        new Item.Settings().group(ItemGroup.MISC)
+    );
+
+    public static void register() {
+        Registry.register(Registry.ITEM, new Identifier("petobject", "lumei_spawn_egg"), LUMEI_SPAWN_EGG);
+    }
+}
